58155ef4b0ab5312641bf1642ffeeef5fc30898b
Theau De La Ruelle <tde-la-r@2G4.42angouleme.fr>
2024-06-21 12:55:04 +0200

    ft_atod corrected

--- libft/sources/ft_atod.c
+++ libft/sources/ft_atod.c
@@ -6,49 +6,50 @@
 /*   By: mhaouas <mhaouas@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 21:34:35 by xeo               #+#    #+#             */
-/*   Updated: 2024/06/20 16:44:24 by tde-la-r         ###   ########.fr       */
+/*   Updated: 2024/06/21 12:54:28 by tde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-static int	atod_format(char *str)
+static char	*atod_format(char *str, int *sign)
 {
-	double	sign;
-
-	sign = 1;
+	*sign = 1;
 	while (*str == ' ' || ('\t' <= *str && *str <= '\r'))
 		str++;
 	if (*str == '-' || *str == '+')
 	{
 		if (*str == '-')
-			sign *= -1;
+			*sign *= -1;
 		str++;
 	}
-	return (sign);
+	return (str);
 }
 
 double	ft_atod(char *str)
 {
 	int		sign;
-	double	nbr;
-	size_t	i;
+	double	integer;
+	double	post_point;
 
-	nbr = 0.0;
-	i = 0;
+	integer = 0.0;
 	if (!str)
 		return (0.0);
-	sign = atod_format(str);
+	str = atod_format(str, &sign);
 	while ((*str >= '0' && *str <= '9') && *str != '.')
-		nbr = nbr * 10 + (*(str++) - '0');
+	{
+		integer = integer * 10 + (*str - '0');
+		str++;
+	}
 	if (*str == '.')
 		str++;
+	post_point = 0.0;
 	while (*str >= '0' && *str <= '9')
 	{
-		nbr = nbr * 10 + (*(str++) - '0');
-		i++;
+		post_point = post_point * 10 + (*str - '0');
+		str++;
 	}
-	while (i-- > 0)
-		nbr /= 10;
-	return (nbr * sign);
+	while (post_point > 1)
+		post_point /= 10;
+	return (integer * sign + post_point * sign);
 }
